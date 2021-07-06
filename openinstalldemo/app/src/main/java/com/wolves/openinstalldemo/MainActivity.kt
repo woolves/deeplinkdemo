219408ba713cb28ef169ac5a7fc09ad40534a45c
kangyangshih <dandanshih1980@gmail.com>
2021-07-06 14:49:37 +0800

    update openinstall demo

--- openinstalldemo/app/src/main/java/com/wolves/openinstalldemo/MainActivity.kt
+++ openinstalldemo/app/src/main/java/com/wolves/openinstalldemo/MainActivity.kt
@@ -2,9 +2,11 @@ package com.wolves.openinstalldemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.fm.openinstall.OpenInstall
+import com.fm.openinstall.listener.AppInstallAdapter
 import com.fm.openinstall.listener.AppWakeUpAdapter
 import com.fm.openinstall.model.AppData
 
@@ -15,16 +17,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // 初使化 openinstall sdk
         OpenInstall.init(this)
-        OpenInstall.getWakeUp(getIntent(), wakeUpAdapter);
-    }
-
-    fun onInstall(appData: AppData) {
-        // 打印数据便于调试
-        println ("getInstall : installData = $appData")
-        // 获取渠道数据
-        val channelCode = appData.getChannel()
-        // 获取自定义数据
-        val bindData = appData.getData()
+        Handler().postDelayed({
+            this.runOnUiThread {
+                //OpenInstall.getWakeUp(getIntent(), wakeUpAdapter)
+                OpenInstall.getInstall(object : AppInstallAdapter() {
+                    override fun onInstall(appData: AppData) {
+                        // 打印数据便于调试
+                        println("getInstall : installData = $appData")
+                        // 获取渠道数据
+                        val channelCode = appData.getChannel()
+                        // 获取自定义数据
+                        val bindData = appData.getData()
+                        val text: TextView = findViewById(R.id.info) as TextView
+                        text.setText(bindData.toString())
+                    }
+                })
+            }
+        }, 1000);
+        //OpenInstall.getWakeUp(getIntent(), wakeUpAdapter);
     }
 
     override fun onNewIntent(intent: Intent?) {
